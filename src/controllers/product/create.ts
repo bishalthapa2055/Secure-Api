15df10581cb7befae96a11022b900174e621dba6
Bishal Thapa <bishalthapab989@gmail.com>
2023-05-02 20:58:55 +0545

    added product schema

---
+++ src/controllers/product/create.ts
@@ -0,0 +1,52 @@
+import { Request, Response } from "express";
+import { Product } from "../../model/product";
+import { BadRequestError } from "../../common/errors/bad-request-error";
+
+const createProduct = async (req: Request, res: Response) => {
+  try {
+    const {
+      name,
+      originalPrice,
+      discountedPrice,
+      discountedPercentage,
+      subDescription,
+      mainDescription,
+    } = req.body;
+    if (
+      !name ||
+      originalPrice ||
+      discountedPercentage ||
+      discountedPrice ||
+      subDescription ||
+      mainDescription
+    ) {
+      throw new BadRequestError("All fields are necessary");
+    }
+    const product = await Product.build({
+      name,
+      originalPrice,
+      discountedPrice,
+      discountedPercentage,
+      subDescription,
+      mainDescription,
+    }).save();
+
+    if (!product) {
+      throw new BadRequestError("Failed to Create Product");
+    }
+    res
+      .status(201)
+      .json({
+        status: true,
+        message: "Product created sucessfully",
+        data: product,
+      });
+  } catch (e) {
+    res.status(400).json({
+      status: false,
+      Error: (e as any).message
+        ? (e as any).message
+        : "Failed to Create . Debug Backend !!!",
+    });
+  }
+};
---
+++ src/model/product.ts
@@ -0,0 +1,88 @@
+import mongoose, { mongo } from "mongoose";
+import { Size } from "../common/types/product-sizes";
+
+export interface ProductAttrs {
+  name: string;
+  originalPrice: number;
+  discountedPrice: number;
+  discountedPercentage: number;
+  //   rating: number;
+  subDescription: string;
+  mainDescription: string;
+  productUrl?: string;
+  size?: Size;
+}
+
+interface ProductModel extends mongoose.Model<ProductDoc> {
+  build(attrs: ProductAttrs): ProductDoc;
+}
+
+export interface ProductDoc extends mongoose.Document, ProductAttrs {
+  name: string;
+  originalPrice: number;
+  discountedPrice: number;
+  discountedPercentage: number;
+  //   rating: number;
+  subDescription: string;
+  mainDescription: string;
+  productUrl?: string;
+  size?: Size;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const productSchema = new mongoose.Schema<ProductDoc>(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    originalPrice: {
+      type: Number,
+      required: true,
+    },
+    discountedPrice: {
+      type: Number,
+      required: true,
+    },
+    discountedPercentage: {
+      type: Number,
+      required: true,
+    },
+    subDescription: {
+      type: String,
+    },
+    mainDescription: {
+      type: String,
+    },
+    productUrl: {
+      type: String,
+      default:
+        "https://bitpointx.s3-ap-southeast-1.amazonaws.com/config/transparent_logo.png",
+    },
+  },
+
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.password;
+        delete ret.__v;
+      },
+    },
+    timestamps: true,
+  }
+);
+productSchema.index({ "$**": "text" });
+
+productSchema.statics.build = (attrs: ProductAttrs) => {
+  return new Product(attrs);
+};
+
+const Product = mongoose.model<ProductDoc, ProductModel>(
+  "Product",
+  productSchema
+);
+
+export { Product };
