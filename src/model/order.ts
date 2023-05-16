e77379e6f5eeb19bb7add39cdc189836bf41f43e
Bishal Thapa <bishalthapab989@gmail.com>
2023-05-16 20:43:33 +0545

    added order model

---
+++ src/model/order.ts
@@ -0,0 +1,62 @@
+import mongoose, { ObjectId, mongo } from "mongoose";
+
+export interface OrderAttrs {
+  userId: ObjectId;
+  productId: ObjectId;
+  orderName: string;
+  orderedQuantity: number;
+}
+interface OrderModel extends mongoose.Model<OrderDoc> {
+  build(attrs: OrderAttrs): OrderDoc;
+}
+
+export interface OrderDoc extends mongoose.Document, OrderAttrs {
+  name: string;
+  userId: ObjectId;
+  productId: ObjectId;
+  orderedQuantity: number;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const orderSchema = new mongoose.Schema<OrderDoc>(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    userId: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+    },
+    productId: {
+      type: mongoose.Types.ObjectId,
+      ref: "Product",
+    },
+    orderedQuantity: {
+      type: Number,
+      required: true,
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.password;
+        delete ret.__v;
+      },
+    },
+    timestamps: true,
+  }
+);
+
+orderSchema.index({ "$**": "text" });
+
+orderSchema.statics.build = (attrs: OrderAttrs) => {
+  return new Order(attrs);
+};
+
+const Order = mongoose.model<OrderDoc, OrderModel>("Order", orderSchema);
+
+export { Order };
