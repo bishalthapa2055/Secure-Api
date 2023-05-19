dee85c2cf6e139fd33ddd200ba2c8518f6a959e1
bishalthapa2055 <bishalthapa2055@gmail.com>
2023-05-19 15:44:23 +0545

    added get-all orders

--- src/routes/v1/order/index.ts
+++ src/routes/v1/order/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { createOrderRouter } from "./create";
+import { getAllOrderRouter } from "./get-all";
 
 const router = Router();
 
 router.use(createOrderRouter);
+router.use(getAllOrderRouter)
 
 export { router as indexOrderRouter };
