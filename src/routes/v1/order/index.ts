bde018158da5719410d58742a9866de26a7b8899
bishalthapa2055 <bishalthapa2055@gmail.com>
2023-05-19 15:55:53 +0545

    added get-one order

--- src/routes/v1/order/index.ts
+++ src/routes/v1/order/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { createOrderRouter } from "./create";
 import { getAllOrderRouter } from "./get-all";
+import { getOneOrderRouter } from "./get-one";
 
 const router = Router();
 
 router.use(createOrderRouter);
-router.use(getAllOrderRouter)
+router.use(getAllOrderRouter);
+router.use(getOneOrderRouter);
 
 export { router as indexOrderRouter };
