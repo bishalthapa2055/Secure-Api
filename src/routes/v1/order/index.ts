07e1de46de6939b220d0f4adcce8c20519998be2
bishalthapa2055 <bishalthapa2055@gmail.com>
2023-05-19 16:12:32 +0545

    added delete order

--- src/routes/v1/order/index.ts
+++ src/routes/v1/order/index.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import { createOrderRouter } from "./create";
 import { getAllOrderRouter } from "./get-all";
 import { getOneOrderRouter } from "./get-one";
+import { deleteOrderRouter } from "./delete";
 
 const router = Router();
 
 router.use(createOrderRouter);
 router.use(getAllOrderRouter);
 router.use(getOneOrderRouter);
+router.use(deleteOrderRouter)
 
 export { router as indexOrderRouter };
